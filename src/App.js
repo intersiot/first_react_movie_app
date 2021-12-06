2c13a0e8b6fccfc941431c6c4ba451179444012b
intersiot <maybeormaybenot.030@gmail.com>
2021-12-06 22:50:48 +0900

    first_react와 이어지는 강의임.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './Button';
+import styles from './App.module.css';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className={styles.title}>Welcome back!!!</h1>
+      {/* <Button text={"Continue"} /> */}
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
