bd3c027b35bb6fe3e15110597bc87a53314cb701
intersiot <maybeormaybenot.030@gmail.com>
2021-12-12 18:14:05 +0900

    .

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import Button from './Button';
 import styles from './App.module.css';
 import {useState, useEffect} from 'react';
 
+function Hello() {
+  function byFn() {
+    console.log("bye :(");
+  }
+  function hiFn() {
+    console.log("hi :)");
+    return byFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("i run only once.");
-  };
-  useEffect(iRunOnlyOnce, []);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      {/* <h1 className={styles.title}>Welcome back!!!</h1> */}
-      {/* <Button text={"Continue"} /> */}
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
